5eab82d98bd6b77941b3375eab5fb4f08162a865
GuiQRSM <quaresmagui20@gmail.com>
2025-12-03 09:37:12 -0300

    add: create a setting for PORT enviroment

--- env/index.ts
+++ env/index.ts
@@ -2,5 +2,6 @@ import 'dotenv/config'
 import { z } from 'zod'
 
 const envScena = z.object({
-    NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev')
+    NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+    PORT: z.coerce.number().default(3333)
 })
