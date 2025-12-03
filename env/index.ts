7e75f78d46ab930f9f73162e7e9606dcb4f97bdd
GuiQRSM <quaresmagui20@gmail.com>
2025-12-03 09:37:59 -0300

    refactor: rename the const

--- env/index.ts
+++ env/index.ts
@@ -1,7 +1,8 @@
 import 'dotenv/config'
 import { z } from 'zod'
 
-const envScena = z.object({
+const envSchema = z.object({
     NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
     PORT: z.coerce.number().default(3333)
 })
+
