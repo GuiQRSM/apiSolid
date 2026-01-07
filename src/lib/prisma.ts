121fbe77dcfb1d9a15dd0ca21547b06568507b04
GuiQRSM <quaresmagui20@gmail.com>
2026-01-07 11:43:09 -0300

    faet: query log only for development environment

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,7 @@
 import { PrismaClient } from '@prisma/client'
+import { env } from 'env/index.ts'
 
-export const prisma = new PrismaClient()
+export const prisma = new PrismaClient({
+  log: env.NODE_ENV === 'dev' ? ['query'] : [],
+})
 console.log('Ok')
