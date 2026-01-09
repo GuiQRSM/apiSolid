504e952a4612dca97c0c80493bf63ce1ce368b0a
GuiQRSM <quaresmagui20@gmail.com>
2026-01-09 09:16:19 -0300

    feat: create an interface for register

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,4 +1,13 @@
-export function registerUseCase() {
+import { prisma } from '@/lib/prisma.ts'
+import { hash } from 'bcryptjs'
+
+interface RegisterUseCaseRequest {
+  name: string
+  email: string
+  password: string
+}
+
+export async function registerUseCase() {
   const password_hash = await hash(password, 6)
 
   const userWithSameEmail = await prisma.user.findUnique({
