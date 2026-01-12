639a1e6ee37cd2c3041b73937e3cf17e3a914448
GuiQRSM <quaresmagui20@gmail.com>
2026-01-12 10:12:46 -0300

    feat: Implement the interface in the Prisma repository to follow the creation logic

--- src/repositories/prisma-users-repository.ts
+++ src/repositories/prisma-users-repository.ts
@@ -2,8 +2,9 @@
 
 import { prisma } from '@/lib/prisma.ts'
 import { Prisma } from '@prisma/client'
+import type { UsersRepository } from './users-repositories.ts'
 
-export class PrismaUsersRepository {
+export class PrismaUsersRepository implements UsersRepository {
   async create(data: Prisma.UserCreateInput) {
     const user = await prisma.user.create({
       data,
