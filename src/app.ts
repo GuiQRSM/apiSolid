d3b1d92275937eebdad3c45b241d416b11ca202b
GuiQRSM <quaresmagui20@gmail.com>
2026-01-13 09:54:34 -0300

    feat: global error handler for catching validation errors

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,19 @@
 
 import fastify from 'fastify'
 import { appRoutes } from './http/routes.ts'
+import { ZodError } from 'zod'
 
 export const app = fastify()
 
 app.register(appRoutes)
+
+// manipulador global de erros para capturar erros de validação do Zod
+app.setErrorHandler((error, request, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error', issues: error.format() })
+  }
+
+  return reply.status(500).send({ message: 'Internal server error' })
+})
