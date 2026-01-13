1234c475ce147823e7568b924a509d4648a76b18
GuiQRSM <quaresmagui20@gmail.com>
2026-01-13 10:03:02 -0300

    feat: detailed error logs in non-production environments.

--- src/app.ts
+++ src/app.ts
@@ -3,17 +3,23 @@
 import fastify from 'fastify'
 import { appRoutes } from './http/routes.ts'
 import { ZodError } from 'zod'
+import { env } from 'env/index.ts'
 
 export const app = fastify()
 
 app.register(appRoutes)
 
 // manipulador global de erros para capturar erros de validação do Zod
-app.setErrorHandler((error, request, reply) => {
+app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
     return reply
       .status(400)
       .send({ message: 'Validation error', issues: error.format() })
+  }
+
+  // log de erros detalhados em ambientes que não sejam de produção
+  if (env.NODE_ENV !== 'production') {
+    console.log(error)
   }
 
   return reply.status(500).send({ message: 'Internal server error' })
