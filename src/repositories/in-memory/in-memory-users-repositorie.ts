90947c9649cd122f4bc4e49c9a079cfed8a5362e
GuiQRSM <quaresmagui20@gmail.com>
2026-01-16 10:00:11 -0300

    feat: In-memory implementation of the user repository for testing purposes.

---
+++ src/repositories/in-memory/in-memory-users-repositorie.ts
@@ -0,0 +1,37 @@
+import type { User, Prisma } from '@prisma/client'
+import type { UsersRepository } from '../users-repositories.ts'
+
+// implementação em memória do repositório de usuários para fins de teste
+export class InMemoryUsersReposity implements UsersRepository {
+  public items: User[] = []
+
+  // procurar um usuário pelo email
+  async findByEmail(email: string) {
+    const user = this.items.find((item) => item.email === email)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+
+  // criar um novo usuário
+  async create(data: Prisma.UserCreateInput) {
+    const user = {
+      id: 'user-1',
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      created_at: new Date(),
+    }
+
+    this.items.push(user)
+
+    return user
+  }
+}
+
+// o repositório mantém uma lista interna de usuários em memória
+// o método findByEmail busca um usuário pelo email na lista interna
+// o método create adiciona um novo usuário à lista interna e retorna o usuário criado
