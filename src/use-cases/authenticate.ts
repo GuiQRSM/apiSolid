f95623015c4146baea64860f7f3716e40e2953f3
GuiQRSM <quaresmagui20@gmail.com>
2026-01-19 10:03:10 -0300

    feat: definition of the interface for the output data of the authentication use case

--- src/use-cases/authenticate.ts
+++ src/use-cases/authenticate.ts
@@ -1,13 +1,18 @@
 import type { UsersRepository } from '@/repositories/users-repositories.ts'
 import { InvalidCredentialsError } from './errors/invalid-credentials-error.ts'
 import { compare } from 'bcryptjs'
+import type { User } from '@prisma/client'
 
+// Definição da interface para os dados de entrada do caso de uso de autenticação
 interface AuthenticateUseCaseRequest {
   email: string
   password: string
 }
 
-interface AuthenticateUSeCaseResponse {}
+// Definição da interface para os dados de saída do caso de uso de autenticação
+interface AuthenticateUSeCaseResponse {
+  user: User
+}
 
 export class AuthenticateUseCase {
   // injeção de dependência do repositório de usuários
