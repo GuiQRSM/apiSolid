7119a04c4728a4e097cb70b6ede63eba7602bd84
GuiQRSM <quaresmagui20@gmail.com>
2026-01-20 10:59:13 -0300

    faet: creates a user in the in-memory repository.

--- src/use-cases/errors/authenticate.spec.ts
+++ src/use-cases/errors/authenticate.spec.ts
@@ -1,6 +1,7 @@
 import { expect, describe, it } from 'vitest'
 import { InMemoryUsersReposity } from '@/repositories/in-memory/in-memory-users-repositorie.ts'
 import { AuthenticateUseCase } from '../authenticate.ts'
+import { hash } from 'bcryptjs'
 
 // teste para o caso de uso de autenticação
 describe('Authenticate UseCase', () => {
@@ -8,15 +9,20 @@ describe('Authenticate UseCase', () => {
   it('should be able to authenticate', async () => {
     // instância do repositório de usuários em memória
     const UsersRepository = new InMemoryUsersReposity()
-    const registerUseCase = new AuthenticateUseCase(UsersRepository)
+    const sut = new AuthenticateUseCase(UsersRepository)
 
-    const { user } = await registerUseCase.execute({
+    await UsersRepository.create({
+      name: 'User test',
+      email: 'usertest@example.com',
+      password_hash: await hash('1123456', 6),
+    })
+
+    const { user } = await sut.execute({
       name: 'User test',
       email: 'usertest@example.com',
       password: '123456',
     })
 
-    //
     expect(user.id).toEqual(expect.any(String))
   })
 })
