2eda311700d5d6190a432b7c1940b0b26f75f509
GuiQRSM <quaresmagui20@gmail.com>
2026-01-20 11:16:19 -0300

    feat: test to verify that it is not possible to authenticate with an incorrect email.

--- src/use-cases/errors/authenticate.spec.ts
+++ src/use-cases/errors/authenticate.spec.ts
@@ -2,10 +2,11 @@ import { expect, describe, it } from 'vitest'
 import { InMemoryUsersReposity } from '@/repositories/in-memory/in-memory-users-repositorie.ts'
 import { AuthenticateUseCase } from '../authenticate.ts'
 import { hash } from 'bcryptjs'
+import { InvalidCredentialsError } from './invalid-credentials-error.ts'
 
 // teste para o caso de uso de autenticação
 describe('Authenticate UseCase', () => {
-  //
+  // teste para verificar se é possível autenticar um usuário com credenciais válidas
   it('should be able to authenticate', async () => {
     // instância do repositório de usuários em memória
     const UsersRepository = new InMemoryUsersReposity()
@@ -23,5 +24,20 @@ describe('Authenticate UseCase', () => {
     })
 
     await expect(user.id).toEqual(expect.any(String))
+  })
+
+  // teste para verificar se não é possível autenticar com emial errada
+  it('should be able to authenticate with wrong email', async () => {
+    // instância do repositório de usuários em memória
+    const UsersRepository = new InMemoryUsersReposity()
+    const sut = new AuthenticateUseCase(UsersRepository)
+
+    // criando um usuário no repositório em memória
+    await expect(() =>
+      sut.execute({
+        email: 'usertest@exam,ple.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError)
   })
 })
