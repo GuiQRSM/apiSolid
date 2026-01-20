1cdd6fb9be93ef9c24aec9f80ae4089168a49b1f
GuiQRSM <quaresmagui20@gmail.com>
2026-01-20 10:52:37 -0300

    refactor: instance AuthenticateUseCase

--- src/use-cases/errors/authenticate.spec.ts
+++ src/use-cases/errors/authenticate.spec.ts
@@ -1,6 +1,6 @@
 import { expect, describe, it } from 'vitest'
-import { RegisterUseCase } from '../register.ts'
 import { InMemoryUsersReposity } from '@/repositories/in-memory/in-memory-users-repositorie.ts'
+import { AuthenticateUseCase } from '../authenticate.ts'
 
 // teste para o caso de uso de autenticação
 describe('Authenticate UseCase', () => {
@@ -8,7 +8,7 @@ describe('Authenticate UseCase', () => {
   it('should be able to authenticate', async () => {
     // instância do repositório de usuários em memória
     const UsersRepository = new InMemoryUsersReposity()
-    const registerUseCase = new RegisterUseCase(UsersRepository)
+    const registerUseCase = new AuthenticateUseCase(UsersRepository)
 
     const { user } = await registerUseCase.execute({
       name: 'User test',
