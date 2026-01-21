5be6c0277ef49c5c701a65bdb57a4ea99ee81cb3
GuiQRSM <quaresmagui20@gmail.com>
2026-01-21 10:27:20 -0300

    feat: declare controller in routes

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,7 +2,9 @@
 
 import type { FastifyInstance } from 'fastify'
 import { register } from './controllers/register.ts'
+import { authenticate } from './controllers/authenticate.ts'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
+  app.post('/sessions', authenticate)
 }
