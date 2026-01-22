28c10cb7aeae4a177d1a3a76988f0a3052f0cc02
GuiQRSM <quaresmagui20@gmail.com>
2026-01-22 09:38:33 -0300

    feat: creating an instance of the authentication use case using the factory pattern

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -2,9 +2,8 @@
 
 import type { FastifyRequest, FastifyReply } from 'fastify'
 import z from 'zod'
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository.ts'
-import { AuthenticateUseCase } from '@/use-cases/authenticate.ts'
 import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error.ts'
+import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case.ts'
 
 export async function authenticate(
   request: FastifyRequest,
@@ -18,10 +17,8 @@ export async function authenticate(
   const { email, password } = authenticateBodySchema.parse(request.body)
 
   try {
-    //  instanciação do repositório de usuários
-    const prismaUsersRepository = new PrismaUsersRepository()
-    // instanciação do use-case de autenticação com injeção de dependência do repositório
-    const authenticateUseCase = new AuthenticateUseCase(prismaUsersRepository)
+    // criação da instância do caso de uso de autenticação com o factory pattern
+    const authenticateUseCase = makeAuthenticateUseCase()
 
     await authenticateUseCase.execute({
       email,
