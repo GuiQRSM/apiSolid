b1448b875154e04be47dea3dfcb5a648a973227a
GuiQRSM <quaresmagui20@gmail.com>
2026-01-22 09:30:04 -0300

    feat: factory to create an instance of the user registration use-case

---
+++ src/use-cases/factories/make-register-use-case.ts
@@ -0,0 +1,12 @@
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository.ts'
+import { RegisterUseCase } from '../register.ts'
+
+// fábrica para criar uma instância do use-case de registro de usuário
+export function makeRegisterUseCase() {
+  //  instanciação do repositório de usuários
+  const prismaUsersRepository = new PrismaUsersRepository()
+  //  instanciação do use-case de registro de usuário com injeção de dependência do repositório
+  const registerUseCase = new RegisterUseCase(prismaUsersRepository)
+
+  return registerUseCase
+}
