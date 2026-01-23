47396daa0a71a6f03133fd8681fa3a1e47c0c6a9
GuiQRSM <quaresmagui20@gmail.com>
2026-01-23 10:20:56 -0300

    feat: method to find a user by ID

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,12 +1,23 @@
 //  repositório de usuários usando Prisma ORM para interagir com o banco de dados
 
 import { prisma } from '@/lib/prisma.ts'
-import { Prisma } from '@prisma/client'
+import { Prisma, type User } from '@prisma/client'
 import type { UsersRepository } from '../users-repositories.ts'
 
 // implementação concreta do repositório de usuários
 // seguindo a abstração definida na interface UsersRepository
 export class PrismaUsersRepository implements UsersRepository {
+  // método para encontrar um usuário pelo ID
+  async findById(id: string): Promise<User | null> {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return user
+  }
+
   // método para encontrar um usuário pelo e-mail
   async findByEmail(email: string) {
     const user = await prisma.user.findUnique({
