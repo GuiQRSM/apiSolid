3910eaa7893a0aa70ce5fa78b4316d71a470db51
GuiQRSM <quaresmagui20@gmail.com>
2026-01-23 09:41:38 -0300

    feat: method to find a user by ID

--- src/repositories/users-repositories.ts
+++ src/repositories/users-repositories.ts
@@ -2,6 +2,7 @@ import { Prisma, type User } from '@prisma/client'
 
 export interface UsersRepository {
   // definição dos métodos que o repositório de usuários deve implementar
+  findById(id: string): Promise<User | null>
   findByEmail(email: string): Promise<User | null>
   create(data: Prisma.UserCreateInput): Promise<User>
 }
