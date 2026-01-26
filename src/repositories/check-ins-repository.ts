31f146bbb42a6a50bd6fc5ef7b7daef2dfa279eb
GuiQRSM <quaresmagui20@gmail.com>
2026-01-26 09:21:59 -0300

    refactor: exchange CheckInCreateInput for CheckInUncheckedCreateInput

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -2,5 +2,5 @@ import { Prisma, type CheckIn } from '@prisma/client'
 
 // Definição da interface do repositório de check-ins
 export interface CheckinsRepository {
-  create(data: Prisma.CheckInCreateInput): Promise<CheckIn>
+  create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
 }
