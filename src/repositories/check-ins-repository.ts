50276b4a6e6a8c840bbc5404c4d3d16b7ce86c93
GuiQRSM <quaresmagui20@gmail.com>
2026-01-27 10:14:26 -0300

    feat: a method to find a check-in by user on a specific date

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -3,4 +3,6 @@ import { Prisma, type CheckIn } from '@prisma/client'
 // Definição da interface do repositório de check-ins
 export interface CheckinsRepository {
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
+  // método para encontrar um check-in por usuário em uma data específica
+  findByUsesrIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
 }
