ffa46812c51f0a57351dd3146203128d772b8fef
GuiQRSM <quaresmagui20@gmail.com>
2026-01-27 09:48:36 -0300

    feat: test to verify if it's not possible to register two check-ins on the same day

--- src/use-cases/factories/check-in.spec.ts
+++ src/use-cases/factories/check-in.spec.ts
@@ -22,7 +22,24 @@ describe('Register CheckIn', () => {
     // asserção para garantir que o check-in foi criado com um ID válido
     expect(checkIn.id).toEqual(expect.any(String))
   })
+
+  // teste para verificar se não é possível registrar dois check-ins no mesmo dia
+  it('should be not able to check in twice in the same day', async () => {
+    await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+    })
+
+    // asserção para garantir que o check-in não foi criado novamente no mesmo dia
+    expect(() =>
+      sut.execute({
+        gymId: 'gym-01',
+        userId: 'user-01',
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
 })
+
 // teste para verificar se o check-in é criado corretamente
 // criação de uma instância do use-case de check-in com um repositório de check-ins falso (mock)
 // execução do use-case de check-in com dados de check-in de teste
