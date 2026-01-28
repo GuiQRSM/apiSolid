9b852c7b59567620e0afa350250a254f7284bfc9
GuiQRSM <quaresmagui20@gmail.com>
2026-01-28 10:04:51 -0300

    feat: method of searching for a gym by ID

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -4,4 +4,15 @@ import type { GymRepository } from '../gyms-repositories.ts'
 // implementação em memória do repositório de academias para fins de teste
 export class InMemoryGymsRepository implements GymRepository {
   public items: Gym[] = []
+
+  // procurar uma academia pelo ID
+  async findById(id: string) {
+    const gym = this.items.find((item) => item.id === id)
+
+    if (!gym) {
+      return null
+    }
+
+    return gym
+  }
 }
